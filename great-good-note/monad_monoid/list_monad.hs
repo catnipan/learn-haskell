805d1dd797259a0ba37b65a9ae97b19f4cbc4189
Yifan Pan <4anpan@gmail.com>
2018-09-26 13:50:01 +0800

    finish list_monad

--- great-good-note/monad_monoid/list_monad.hs
+++ great-good-note/monad_monoid/list_monad.hs
@@ -41,5 +41,30 @@ guard False = mzero
 
 -- [1..50] >>= (\x -> guard ('7' `elem` show x) >> return x)
 -- [7,17,27,37,47]
--- 当 x 为 6 时，guard 函数返回 return ()
--- (\)
+-- 当 x 为 6 时，guard 函数返回 mzero 即 []
+-- (\x -> ([] >> return x)) 6
+-- (\x -> ([] >> \_ -> return x)) 6
+-- (\x -> []) 6
+-- []
+-- concat 这个 [] 就相当于过滤掉了
+
+-- 当 x 为 7 时，guard 函数返回 return ()
+-- (\x -> (return () >> return x)) 7
+-- (\x -> ([()] >>= (\_ -> return x))) 7
+-- (\x -> [x]) 7
+-- [7]
+
+-- guard (5 > 2) >> return "cool" :: [String]
+-- ["cool"]
+-- guard (1 > 2) >> return "cool" :: [String]
+-- []
+-- guard 成功是一个 [()]，空的 tuple 不起作用，
+-- 使右边的继续进行
+-- 失败则是一个空的 []
+-- 本质在于 [] >> 和 [()] >> 表现不一样
+
+sevensOnly :: [Int]
+sevensOnly = do
+  x <- [1..50]
+  guard ('7' `elem` show x)
+  return x
