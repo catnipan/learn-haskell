ef18a1c9d7a117881f6e15b1355139447a44f68f
Yifan Pan <4anpan@gmail.com>
2018-09-28 23:24:06 +0800

    add something for fillPlaceHolder

--- algorithm/list.hs
+++ algorithm/list.hs
@@ -1,2 +1,4 @@
-fillPlaceHolder :: [Maybe a] -> [Maybe a] -> [Maybe a]
-fillPlaceHolder [Just 4, Just 5, Nothing, Just 3, Nothing, Just 4] [Just 3, Just 4]
+fillPlaceHolder :: [Maybe a] -> [Maybe a] -> [Maybe a] -> [Maybe a]
+fillPlaceHolder finalArgs ((Just x):xs) (ys) =
+  
+-- fillPlaceHolder [Just 4, Just 5, Nothing, Just 3, Nothing, Just 4] [Just 3, Just 4]
