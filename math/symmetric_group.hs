bcea3417335098404e5e6058daaa51b32a50b08e
Yifan Pan <4anpan@gmail.com>
2018-09-28 14:07:38 +0800

    finish symmetric_group

--- math/symmetric_group.hs
+++ math/symmetric_group.hs
@@ -1,43 +1,52 @@
 import Data.List
 
-type PermuGroup = [Int]
-type SearchList = [(Int, Int)]
+type Pair = (Int,Int)
+type PermutationGroup = [Int]
 
-newtype PermuItem = PermuItem { getPermuItem :: [PermuGroup] }
+newtype Permutation = Permutation { getPermutation :: [Int] }
 
-showPermuGroup :: PermuGroup -> String
-showPermuGroup (_:[]) = ""
-showPermuGroup groups = "(" ++ (mconcat . intersperse "," . map show $ groups) ++ ")"
+instance Semigroup Permutation where
+  Permutation pa <> Permutation pb = Permutation . map ((pb !!) . (subtract 1)) $ pa
 
-instance Show PermuItem where
-  show (PermuItem permuGroups) =
-    let str = mconcat . map showPermuGroup $ permuGroups
-    in if str == "" then "(1)" else str
+instance Show Permutation where
+  show (Permutation permutation) = if str == "" then "(1)" else str
+    where
+      str = mconcat . map showPermuGroup . pairListToPermutationGroupList . zip [1..] $ permutation
+      showPermuGroup :: PermutationGroup -> String
+      showPermuGroup (_:[]) = ""
+      showPermuGroup groups = "(" ++ (mconcat . intersperse "," . map show $ groups) ++ ")"
 
-startPermuOn :: Int -> SearchList -> (PermuGroup, SearchList)
-startPermuOn firstStart searchList = (permuGroup', searchList')
-  where 
-    calcOnStart :: Int -> PermuGroup -> PermuGroup
-    calcOnStart start permuGroup = case lookup start searchList of
-      Nothing -> permuGroup
-      Just thenStart ->
-        if thenStart == firstStart
-          then permuGroup
-          else calcOnStart thenStart (thenStart:permuGroup)
-    searchList' = filter (not . (`elem` permuGroup') . fst) searchList
-    permuGroup' = reverse $ calcOnStart firstStart [firstStart]
-
-searchListToPermuGroupList :: SearchList -> PermuItem
-searchListToPermuGroupList startSearchList = iterator startSearchList $ PermuItem []
+pairListToPermutationGroupList :: [Pair] -> [PermutationGroup]
+pairListToPermutationGroupList pairList = iterator pairList []
   where
-    iterator :: SearchList -> PermuItem -> PermuItem
-    iterator searchList@((start,_):_) (PermuItem permuItem) =
+    iterator :: [Pair] -> [PermutationGroup] -> [PermutationGroup]
+    iterator searchList@((start,_):_) permuItem =
       let (newPermuGroup, newSearchList) = startPermuOn start searchList
-      in iterator newSearchList (PermuItem (newPermuGroup:permuItem))
+      in iterator newSearchList (newPermuGroup:permuItem)
     iterator _ permuItem = permuItem
+    startPermuOn :: Int -> [Pair] -> (PermutationGroup, [Pair])
+    startPermuOn firstStart searchList = (permuGroup', searchList')
+      where 
+        calcOnStart :: Int -> PermutationGroup -> PermutationGroup
+        calcOnStart start permuGroup = case lookup start searchList of
+          Nothing -> permuGroup
+          Just thenStart ->
+            if thenStart == firstStart
+              then permuGroup
+              else calcOnStart thenStart (thenStart:permuGroup)
+        searchList' = filter (not . (`elem` permuGroup') . fst) searchList
+        permuGroup' = reverse $ calcOnStart firstStart [firstStart]
 
-symmetricGroupN :: Int -> [PermuItem]
-symmetricGroupN n = map getPermuItemOnPermutation $ permutations [1..n]
-  where
-    getPermuItemOnPermutation :: [Int] -> PermuItem
-    getPermuItemOnPermutation permu = searchListToPermuGroupList $ zip [1..n] permu
+makePermutation :: [Int] -> Permutation
+makePermutation xs
+  | sort xs == [1..length xs] = Permutation xs
+  | otherwise = undefined
+
+symmetricGroupN :: Int -> [Permutation]
+symmetricGroupN n = map Permutation . permutations $ [1..n]
+
+symmetricGroup3 = symmetricGroupN 3
+
+compositionTable :: (Semigroup m, Show m) => [m] -> IO ()
+compositionTable semigroup = mapM_ putStrLn [(showCalc a b) | a <- semigroup, b <- semigroup]
+  where showCalc a b = (show a ++ " <> " ++ show b ++ " = " ++ show (a <> b))
