78d1ebad195c2d0725dd3436775123c71945de3f
Yifan Pan <4anpan@gmail.com>
2018-09-28 15:31:34 +0800

    finish state_monad

--- great-good-note/monad_monoid/state_monad.hs
+++ great-good-note/monad_monoid/state_monad.hs
@@ -1,3 +1,4 @@
+import System.Random
 import Control.Monad.State
 
 type Stack = [Int]
@@ -18,23 +19,100 @@ _stackMainp stack = let
 
 -- instance Monad (State s) where
 --   return x = State $ \s -> (x,s)
---   (State h) >>= f = State $ \s -> let (a, newState) = h s
---                                       (State g) = f a
---                                   in g newState
+--   (State h) >>= f = State $ \s ->
+--     let (a, newState) = h s
+--         (State g) = f a
+--     in g newState
+
+-- (>>=) :: Monad m => m a -> (a -> m b) -> m b
+-- 这里 m 是 State s
+-- (>>=) :: State s a -> (a -> State s b) -> State s b
+-- f :: a -> State s b
+-- 首先我们要得到参数 a
+-- 即 (a, newState) = h s
+-- f a 是 (State g) :: State s b
+-- g :: s -> (b, s)
 
 pop :: State Stack Int
 pop = state $ \(x:xs) -> (x,xs)
 
+getStack' :: State Stack Stack
+getStack' = state $ \s -> (s,s)
+
+putStack' :: Stack -> State Stack ()
+putStack' newState = state $ \_ -> ((), newState)
+
+readTop :: State Stack Int
+readTop = state $ \ls@(x:_) -> (x, ls)
+
 push :: Int -> State Stack ()
 push a = state $ \xs -> ((), a:xs)
 
-stackMainp :: State Stack Int
-stackMainp = do
-  push 3
-  push 5
-  push 99
+reverseStack4 :: State Stack ()
+reverseStack4 = do
   a <- pop
-  pop
+  b <- pop
+  c <- pop
+  d <- pop
+  push a
+  push b
+  push c
+  push d
+  return ()
 
--- runState stackMainp [5,8,2,1]
--- (5,[8,2,1])
+-- runState reverseStack4 [5,8,2,1]
+
+ifnot5push38 :: State Stack ()
+ifnot5push38 = do
+  a <- readTop
+  if a == 5
+    then return ()
+    else do
+      push 3
+      push 8
+
+-- runState ifnot5push38 [5,8,2,1]
+-- runState ifnot5push38 [1,2,3]
+
+ifTopIs100ThenReverse :: State Stack ()
+ifTopIs100ThenReverse = do
+  a <- readTop
+  if a == 100
+    then reverseStack4
+    else return ()
+
+-- runState ifTopIs100ThenReverse [100,3,4,5,6,4,3]
+-- ((),[5,4,3,100,6,4,3])
+
+stackyStack :: State Stack ()
+stackyStack = do
+  stackNow <- get
+  if stackNow == [1,2,3]
+    then (put [8,3,1])
+    else (put [9,2,1])
+
+stackyStack' :: State Stack ()
+stackyStack' = do
+  stackNow <- getStack'
+  if stackNow == [1,2,3]
+    then (putStack' [8,3,1])
+    else (putStack' [9,2,1])
+
+
+-- 使用 State Monad 处理随机性
+
+-- random :: (Random a, RandomGen g) => g -> (a, g)
+-- RandomGen g 是“状态”
+
+randomSt :: (RandomGen g, Random a) => State g a
+randomSt = state random
+
+threeCoins :: State StdGen (Bool, Bool, Bool)
+threeCoins = do
+  a <- randomSt
+  b <- randomSt
+  c <- randomSt
+  return (a,b,c)
+
+-- runState threeCoins (mkStdGen 33)
+-- ((True,False,True),680029187 2103410263)
