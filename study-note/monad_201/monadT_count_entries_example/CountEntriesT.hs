8f778615209df0666841c86c3a6ef92082754e01
Yifan Pan <4anpan@gmail.com>
2018-10-10 18:16:03 +0800

    add monadT example

--- study-note/monad_201/monadT_count_entries_example/CountEntriesT.hs
+++ study-note/monad_201/monadT_count_entries_example/CountEntriesT.hs
@@ -6,11 +6,15 @@ import Control.Monad (forM_, when)
 import Control.Monad.Trans (liftIO)
 import Control.Monad.Writer (WriterT, tell, runWriterT)
 
+-- newtype WriterT w m a = WriterT { runWriterT :: m (a, w) }
 -- WriterT :: m (a, w) -> WriterT w m a
 -- runWriterT :: WriterT w m a -> m (a, w)
 -- execWriterT :: Monad m => WriterT w m a -> m w
 
 countEntries :: FilePath -> WriterT [(FilePath, Int)] IO ()
+-- w is [(FilePath, Int)]
+-- m is IO
+-- a is ()
 countEntries path = do
   contents <- liftIO . listDirectory $ path
   tell [(path, length contents)]
@@ -18,3 +22,11 @@ countEntries path = do
     let newName = path </> name
     isDir <- liftIO . doesDirectoryExist $ newName
     when isDir $ countEntries newName
+
+-- liftIO :: MonadIO m => IO a -> m a
+-- listDirectory :: FilePath -> IO [FilePath]
+-- listDirectory path :: IO [FilePath]
+-- liftIO (listDirectory path) :: ??
+
+-- runWriterT $ countEntries "/aaa"
+-- ((), [("/aaa", 3), ("/aaa/bbb", 4)])
