370b07ad7f3ec1a7ea847dd25a7f73eca079c33a
Yifan Pan <4anpan@gmail.com>
2018-10-12 11:51:21 +0800

    add calcHomomorphismFromZnToZm

--- math/Zn_Zm_homomorphism.hs
+++ math/Zn_Zm_homomorphism.hs
@@ -29,10 +29,14 @@ times_ x (Z r v)= (Z 18) . (* x) . (`mod` r) $ v
 
 type ErrorCase = String
 
-testHomomorphism :: [Z] -> ZZMapping -> [ErrorCase]
-testHomomorphism group zzmp = allCases >>= testCase
+listZ12Mapping :: ZZMapping -> [String]
+listZ12Mapping zzmp = map getMapInfoStr z12Group
+  where getMapInfoStr z12 = show z12 ++ " -> " ++ show (zzmp z12)
+
+testZ12Homomorphism :: ZZMapping -> [ErrorCase]
+testZ12Homomorphism zzmp = allCases >>= testCase
   where
-    allCases = (,) <$> group <*> group
+    allCases = (,) <$> z12Group <*> z12Group
     testCase (z1@(Z 12 _), z2@(Z 12 _)) = 
       if ma /= mb then [ "x\t" ++ maStr ++ "\t" ++ mbStr ] else []
       where ma = zzmp (z1 <> z2)
@@ -40,7 +44,17 @@ testHomomorphism group zzmp = allCases >>= testCase
             mb = (zzmp z1) <> (zzmp z2)
             mbStr = "φ(" ++ show z1 ++ ")+φ(" ++ show z2 ++ ") = " ++ show mb
 
--- mapM putStrLn $ testHomomorphism z12Group (times_ 2)
--- mapM putStrLn $ testHomomorphism z12Group (times_ 3)
+-- mapM putStrLn $ testZ12Homomorphism (times_ 2)
+-- mapM putStrLn $ testZ12Homomorphism (times_ 3)
 -- x_ is ZZMapping creator from Z12Group to Z18Group
 -- when take 0,9,6,12,3,15 it is homomorphism
+
+-- calculate (times_ ?) homomorphism from (Zn, +) to (Zm, +)
+-- 1(from Zn) * n = 0 (from Zn)
+-- φ(1)(from Zm) * n = 0 (from Zm) (because of homomorphism)
+-- so n is divisible by the rank of φ(1)
+-- whereas |φ(1)| = m / (m, φ(1))
+calcHomomorphismFromZnToZm :: Rank -> Rank -> [Int]
+calcHomomorphismFromZnToZm r1 r2 = filter isGood [0..(r2-1)]
+  where isGood x = (r2 `quot` (gcd r2 x)) `isZC` r1
+        x `isZC` y = y `mod` x == 0
