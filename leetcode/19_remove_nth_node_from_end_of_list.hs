9ae7e301898791fe87e4b396b3299f9fc00ee9ee
Yifan Pan <4anpan@gmail.com>
2018-10-19 18:19:16 +0800

    add linkedList

---
+++ leetcode/19_remove_nth_node_from_end_of_list.hs
@@ -0,0 +1,21 @@
+-- Given a linked list, remove the n-th node from the end of list and return its head.
+
+-- Example:
+
+-- Given linked list: 1->2->3->4->5, and n = 2.
+
+-- After removing the second node from the end, the linked list becomes 1->2->3->5.
+-- Note:
+
+-- Given n will always be valid.
+
+-- Follow up:
+
+-- Could you do this in one pass?
+
+-- removeNthFromEnd :: [a] -> Int -> [a]
+
+import MyUtils.LinkedList
+
+removeNthFromEnd :: LinkedList a -> Int -> LinkedList a
+removeNthFromEnd headNode nth = undefined
