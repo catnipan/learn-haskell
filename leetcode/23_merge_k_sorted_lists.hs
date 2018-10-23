ae328275e84646e66d817a714d28d40797fd0af1
Yifan Pan <4anpan@gmail.com>
2018-10-23 11:37:17 +0800

    solution#23

---
+++ leetcode/23_merge_k_sorted_lists.hs
@@ -0,0 +1,28 @@
+-- Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+-- Example:
+
+-- Input:
+-- [
+--   1->4->5,
+--   1->3->4,
+--   2->6
+-- ]
+-- Output: 1->1->2->3->4->4->5->6
+
+{-# LANGUAGE ScopedTypeVariables #-}
+
+mergeKLists :: forall a. (Ord a) => [[a]] -> [a]
+mergeKLists xss =
+  case bubbleXss xss of
+    [] -> []
+    ((sx:srxs):rxss) -> sx:(mergeKLists $ srxs:rxss)
+  where
+    bubbleXss :: [[a]] -> [[a]]
+    bubbleXss xss = foldr bubble [] xss
+    bubble :: [a] -> [[a]] -> [[a]]
+    bubble [] nxss = nxss
+    bubble xs' [] = [xs']
+    bubble xs'@(x':_) nxss@(xs@(x:_):rst)
+      | x < x' = xs:xs':rst
+      | otherwise = xs':nxss
