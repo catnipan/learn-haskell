c17f2be777446baf7e0ae61b96347f2b61de3d41
Yifan Pan <4anpan@gmail.com>
2018-10-25 18:13:36 +0800

    op#1

--- leetcode/1_two_sum.hs
+++ leetcode/1_two_sum.hs
@@ -9,19 +9,40 @@
 -- Because nums[0] + nums[1] = 2 + 7 = 9,
 -- return [0, 1].
 
+import qualified Data.Map as Map
+import Control.Monad.Writer(Writer, writer, runWriter, tell)
+import Control.Applicative((<|>))
+
 type Indice = Int
+type NumIndiceMap = Map.Map Int Indice
+
+isHalfOf :: Int -> Int -> Bool
+x `isHalfOf` y
+  | even y = y `quot` 2 == x
+  | otherwise = False
+
+type FoldWriter = Writer [Int] NumIndiceMap
 
 twoSum :: [Int] -> Int -> Maybe (Indice, Indice)
-twoSum xs target = getFirstIndice indicesAnswers
+twoSum xs target =
+  foldr (<|>) (getHalfAnswer halfIdxs) $ map toMaybeIndice idxXss
   where
-    idxXs = zip [0..] xs
-    indicesAnswers :: [(Indice, Indice)]
-    indicesAnswers = do
-      (idxA, a) <- idxXs
-      (idxB, b) <- drop idxA idxXs
-      if a + b == target then [(idxA, idxB)] else []
-    getFirstIndice (ij:_) = Just ij
-    getFirstIndice [] = Nothing
+    idxXss = zip [0..] xs
+    (numIndiceMap, halfIdxs) =
+      runWriter $ foldr addToMap (writer (Map.empty, [])) idxXss
+    getHalfAnswer (i:j:_) = Just (i,j)
+    getHalfAnswer _ = Nothing
+    addToMap :: (Indice, Int) -> FoldWriter -> FoldWriter
+    addToMap (idx, int) ws = do
+      oldMap <- ws
+      if int `isHalfOf` target
+        then do { tell [idx]; return oldMap }
+        else return $ Map.insert int idx oldMap
+    toMaybeIndice :: (Indice, Int) -> Maybe (Indice, Indice)
+    toMaybeIndice (i, v) =
+      case Map.lookup (target - v) numIndiceMap of
+        Just j -> Just (i, j)
+        _ -> Nothing
 
 -- twoSum [2, 7, 11, 15] 9
 -- Just (0,1)
